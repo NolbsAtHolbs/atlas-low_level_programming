8a993752a4d06a13d954dfd901e54e91f8a513c8
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-09 13:40:32 -0600

    default print statement spot

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -18,12 +18,7 @@ int interpolation_search(int *array, size_t size, int value)
 	{ /* calculate position using interpolation formula */
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 					 * (value - array[low]));
-
-		if (pos >= size)
-		{
-			printf("Value checked array[%lu] is out of range\n", pos);
-			break; /* stop searching here */
-		} /* print the comparison action */
+		/* print the comparison action */
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
@@ -32,5 +27,6 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			high = pos - 1;
 	}
+	printf("Value checked array[%lu] is out of range\n", pos);
 	return (-1);
 }
