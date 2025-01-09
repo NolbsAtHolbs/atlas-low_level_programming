37297de4ecf4d5b559042a355f18d766e72c191e
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-09 13:54:18 -0600

    elses

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -18,14 +18,13 @@ int interpolation_search(int *array, size_t size, int value)
 	pos = low + (((double)(high - low) / (array[high] - array[low]))
 		  * (value - array[low]));
 	while (low < high && value >= array[low] && value <= array[high])
-	{
-		/* print the comparison action */
+	{ /* print the comparison action */
 		printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 		if (array[pos] == value)
 			return (pos);
 		if (array[pos] < value)
 			low = pos + 1;
-		else
+		else if (array[pos] > value)
 			high = pos - 1;
 	}
 	printf("Value checked array[%lu] is out of range\n", pos);
