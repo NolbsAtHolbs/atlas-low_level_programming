ff7d51d0919414f578317e5984d246d65f556238
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-09 13:17:51 -0600

    change high value on condition

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -35,7 +35,7 @@ int interpolation_search(int *array, size_t size, int value)
 		if (array[pos] < value)
 			low = pos + 1;
 		else
-			high = pos - 1;
+			high = pos;
 	}
 	if (low < size && array[low] == value)
 		return ((int)low); /* last check if low is w/in bounds & matches val */
