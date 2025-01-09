5b8335e6f93b6713e05e58b905e13419a05a4d84
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-09 13:46:18 -0600

    altered while condition

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -14,7 +14,7 @@ int interpolation_search(int *array, size_t size, int value)
 
 	if (!array || size == 0)
 		return (-1);
-	while (low <= high && value >= array[low] && value <= array[high])
+	while (low < high && value >= array[low] && value <= array[high])
 	{ /* calculate position using interpolation formula */
 		pos = low + (((double)(high - low) / (array[high] - array[low]))
 					 * (value - array[low]));
