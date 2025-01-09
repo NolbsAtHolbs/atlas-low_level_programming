538232f38bbffff84b232c7ef7fb26cfef897bf2
NolbsAtHolbs <NolbsAtHolbs48725501@gmail.com>
2025-01-09 14:15:54 -0600

    finally

--- search_algorithms/102-interpolation.c
+++ search_algorithms/102-interpolation.c
@@ -12,7 +12,7 @@ int interpolation_search(int *array, size_t size, int value)
 {
 	size_t pos = 0, low = 0, high = size - 1;
 
-	if (!array)
+	if (!array || size == 0)
 		return (-1);
 	pos = low + (((double)(high - low) / (array[high] - array[low]))
 		  * (value - array[low])); /* find pos using interpolation formula */
